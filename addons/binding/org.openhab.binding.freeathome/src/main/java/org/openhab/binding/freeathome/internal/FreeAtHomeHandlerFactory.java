af6b9eb439f06db560d11024d910ced4073b77a5
ruebox <ruebox@users.noreply.github.com>
2018-03-18 01:06:52 +0100

    Fixed some mvn stuff

--- addons/binding/org.openhab.binding.freeathome/src/main/java/org/openhab/binding/freeathome/internal/FreeAtHomeHandlerFactory.java
+++ addons/binding/org.openhab.binding.freeathome/src/main/java/org/openhab/binding/freeathome/internal/FreeAtHomeHandlerFactory.java
@@ -1,5 +1,4 @@
 /**
- /**
  * Copyright (c) 2014-2018 by the respective copyright holders.
  *
  * All rights reserved. This program and the accompanying materials
@@ -9,13 +8,12 @@
  */
 package org.openhab.binding.freeathome.internal;
 
-import static org.openhab.binding.freeathome.FreeAtHomeBindingConstants.*;
-
 import org.eclipse.smarthome.core.thing.Bridge;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.openhab.binding.freeathome.FreeAtHomeBindingConstants;
 import org.openhab.binding.freeathome.handler.FreeAtHomeBridgeHandler;
 import org.openhab.binding.freeathome.handler.FreeAtHomeDummyHandler;
 import org.openhab.binding.freeathome.handler.FreeAtHomeRaffStoreHandler;
@@ -39,7 +37,7 @@ public class FreeAtHomeHandlerFactory extends BaseThingHandlerFactory {
 
     @Override
     public boolean supportsThingType(ThingTypeUID thingTypeUID) {
-        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+        return FreeAtHomeBindingConstants.SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
     }
 
     @Override
@@ -47,41 +45,41 @@ public class FreeAtHomeHandlerFactory extends BaseThingHandlerFactory {
 
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
 
-        if (thingTypeUID.equals(RAFFSTORE_THING_TYPEUID)) {
+        if (thingTypeUID.equals(FreeAtHomeBindingConstants.RAFFSTORE_THING_TYPEUID)) {
             return new FreeAtHomeRaffStoreHandler(thing);
         }
 
-        if (thingTypeUID.equals(BRIDGE_THING_TYPEUID)) {
+        if (thingTypeUID.equals(FreeAtHomeBindingConstants.BRIDGE_THING_TYPEUID)) {
             logger.debug("create BridgeHandler");
             FreeAtHomeBridgeHandler handler = new FreeAtHomeBridgeHandler((Bridge) thing);
             return handler;
         }
 
-        if (thingTypeUID.equals(SCENE_THING_TYPEUID)) {
+        if (thingTypeUID.equals(FreeAtHomeBindingConstants.SCENE_THING_TYPEUID)) {
             logger.debug("create Scene Handler");
             FreeAtHomeSceneHandler handler = new FreeAtHomeSceneHandler(thing);
             return handler;
         }
 
-        if (thingTypeUID.equals(SCENARIO_SELECTOR_THING_TYPEUID)) {
+        if (thingTypeUID.equals(FreeAtHomeBindingConstants.SCENARIO_SELECTOR_THING_TYPEUID)) {
             logger.debug("create Scenario Selector Handler");
             FreeAtHomeScenarioSelectorHandler handler = new FreeAtHomeScenarioSelectorHandler(thing);
             return handler;
         }
 
-        if (thingTypeUID.equals(SWITCH_THING_TYPEUID)) {
+        if (thingTypeUID.equals(FreeAtHomeBindingConstants.SWITCH_THING_TYPEUID)) {
             logger.debug("create Switch Handler");
             FreeAtHomeSwitchHandler handler = new FreeAtHomeSwitchHandler(thing);
             return handler;
         }
 
-        if (thingTypeUID.equals(THERMOSTAT_THING_TYPEUID)) {
+        if (thingTypeUID.equals(FreeAtHomeBindingConstants.THERMOSTAT_THING_TYPEUID)) {
             logger.debug("create thermostat Handler");
             FreeAtHomeThermostatHandler handler = new FreeAtHomeThermostatHandler(thing);
             return handler;
         }
 
-        if (thingTypeUID.equals(DUMMY_THING_TYPEUID)) {
+        if (thingTypeUID.equals(FreeAtHomeBindingConstants.DUMMY_THING_TYPEUID)) {
             logger.debug("create dummy Handler");
             FreeAtHomeDummyHandler handler = new FreeAtHomeDummyHandler(thing);
             return handler;
